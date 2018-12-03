922f5c24b480c740feeb7bee0189f7f2e41fca4e
milkyway <sea_bom_12@park012241.me>
2018-12-03 18:53:02 +0900

    Edit: 헤더 퍼블리싱 작업 완료

--- FrontEnd/minibirds/src/Components/Header.js
+++ FrontEnd/minibirds/src/Components/Header.js
@@ -5,15 +5,16 @@ import logo from './img/MiniBirds-Logo.png'
 
 const Header = ({onTwit, logOut, onKeyPress}) => {
     return (
-        <div className="header">
+        <div className="main-header">
             <div className="home" onKeyPress={onKeyPress}>
                 <img src={Home} alt="홈" className="home-img"></img>
+                <div className="home-letter">홈</div>
             </div>
             <div className="main">
                 <img src={logo} alt="로고" className="logo-img"></img>
             </div>
-            <div className="header_right">
-                <ul>
+            <div className="header-right">
+                <ul className="twit_box">
                     <li className="twit" onTwit={onTwit}>트윗하기</li>
                     <li className="twit" logOut={logOut}>로그아웃</li>
                 </ul>
