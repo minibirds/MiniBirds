1fb866cce5ce71180fc1339cff4afcc6207a1f0d
milkyway <sea_bom_12@park012241.me>
2018-12-05 11:32:24 +0900

    Add: 프로필 컴포넌트 퍼블리싱 완료

--- FrontEnd/minibirds/src/App.js
+++ FrontEnd/minibirds/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Components/Header.js';
+import Profile from './Components/profile.js';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Profile />
       </div>
     );
   }
