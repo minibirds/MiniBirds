b851fba14e49127467c65c00642fd59bb57fa720
milkyway <sea_bom_12@park012241.me>
2018-12-08 23:41:35 +0900

    Add: 탭바 컴포넌트 완성

--- FrontEnd/minibirds/src/Components/profile.js
+++ FrontEnd/minibirds/src/Components/profile.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import editimg from './img/edit-img.png';
+import Tapba from './Tapba.js';
 import './CSS/profile.css';
 
 const Profile = ({id, name, img, edit, following, follower, twitList}) => {
     return (
-        <div className="main-profile">
-            <div className="big-box">
-                <div className="small-box">
-                    <img src={img} />
+        <div>
+            <div className="main-profile">
+                <div className="big-box">
+                    <div className="small-box">
+                        <img src={img} />
+                    </div>
+                </div>
+                <div className="text-box">
+                    <div className="nick-name">히히낙낙</div>
+                    <div className="user-id">@dsasds</div>
+                    <div className="info">퉤ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ</div>
+                    <div className="edit-btn">
+                        <img src={editimg} alt="수정" className="edit-img" />
+                        <div className="edit-letter">프로필 수정하기</div>
+                    </div>
                 </div>
             </div>
-            <div className="text-box">
-                <div className="nick-name">히히낙낙</div>
-                <div className="user-id">@dsasds</div>
-                <div className="info">퉤ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ</div>
-                <div className="edit-btn">
-                    <img src={editimg} alt="수정" className="edit-img" />
-                    <div className="edit-letter">프로필 수정하기</div>
-                </div>
-            </div>
+            <Tapba />
         </div>
     );
 }
