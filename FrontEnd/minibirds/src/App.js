7d83854e903522ce76073c9400a1c1299b2188f3
milkyway <sea_bom_12@park012241.me>
2018-12-13 19:34:09 +0900

    Edit: 마지 충돌 해결을 위해 App.js 비움

--- FrontEnd/minibirds/src/App.js
+++ FrontEnd/minibirds/src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Header from './Components/Header';
-import Profile from './Components/Profile.js';
-import Tapba from './Components/Tapba.js';
-import Twitlist from './Components/Twitlist.js';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header Form={<Profile Tapba={<Tapba twitlist={<Twitlist />}/>} />}/>
       </div>
     );
   }
