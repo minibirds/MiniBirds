511c6dac88e15a56d35b75f5fa7cec890f43fa14
huewilliams <4879sinwoo@naver.com>
2018-12-26 23:44:50 +0900

    CREATE: express server

---
+++ BackEnd/code/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const session = require('express-session');
+
+const indexRouter = require('./routes');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','pug');
+app.set('port', process.env.PORT || 5000);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+app.use(cookieParser('boostrun'));
+app.use(session({
+    resave: false,
+    saveUninitialized : false,
+    secret : 'boostrun',
+    cookie : {
+        httpOnly : true,
+        secure : false,
+    }
+}));
+
+app.use('/', indexRouter);
+
+// 해당 라우터가 없을시 404 Error 발생
+app.use((req, res, next)=>{
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// 에러 핸들러
+app.use((err, req, res) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render(error);
+});
+
+app.listen(app.get('port'), ()=> {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
