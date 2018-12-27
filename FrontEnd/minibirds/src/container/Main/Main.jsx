48e13d42d4b0e8a0606a8ebdeddebed2195b162b
soheejang15 <jsh167528@gmail.com>
2018-12-27 20:14:54 +0900

    MODIFY : 메인 컴포넌트
    트윗하기와 트윗리스트 추가 및 스타일 수정

--- FrontEnd/minibirds/src/container/Main/Main.jsx
+++ FrontEnd/minibirds/src/container/Main/Main.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import Introduction from '../../Components/Main/Introduction';
 import MainTweet from '../../Components/Main/MainTweet';
 import MiniProfile from '../../Components/Main/MiniProfile';
+import TwitList from '../../Components/Main/Twitlist';
 import './Main.css';
 
 const Main = () =>{
   return(
     <div className='main-page'>
       <MiniProfile/>
-      <MainTweet/>
+      <div className="main-middle">
+        <MainTweet/>
+        <TwitList/>
+      </div>
       <Introduction/>
     </div>
   );
