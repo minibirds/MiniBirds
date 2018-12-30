d76ea7f6b638ac623336666aa57d49d42b5efa98
soheejang15 <jsh167528@gmail.com>
2018-12-30 16:21:01 +0900

    ADD : img태그 onClick 속성
    img 태그 클릭 시 새로고침

--- FrontEnd/minibirds/src/Components/Header.js
+++ FrontEnd/minibirds/src/Components/Header.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './CSS/Header.css';
 import Home from './img/home-button.png'
 import logo from './img/MiniBirds-Logo.png'
+import {Link} from 'react-router-dom';
 
 const Header = ({onTwit, logOut, onKeyPress, Form}) => {
     return (
         <div>
             <div className="main-header">
-                <div className="home" onKeyPress={onKeyPress}>
-                    <img src={Home} alt="홈" className="home-img"></img>
+                <div className="home"  onClick={() => window.location.reload()}>
+                    <img src={Home} alt="홈" className="home-img" ></img>
                     <div className="home-letter">홈</div>
                 </div>
                 <div className="main">
-                    <img src={logo} alt="로고" className="logo-img"></img>
+                    <img src={logo} alt="로고" className="logo-img"  onClick={() => window.location.reload()}></img>
                 </div>
                 <div className="header-right">
                     <ul className="twit_box">
