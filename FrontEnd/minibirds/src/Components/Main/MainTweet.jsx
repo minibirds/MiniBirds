fac785e142920046cf8c412ac27b439cd6a9ce0a
soheejang15 <jsh167528@gmail.com>
2018-12-30 16:24:54 +0900

    MODIFY : 프로필 이미지

--- FrontEnd/minibirds/src/Components/Main/MainTweet.jsx
+++ FrontEnd/minibirds/src/Components/Main/MainTweet.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './MainTweet.css'
-import Logo from '../img/MiniBirds-Logo.png'
+import ProfileImg from '../img/profile-img-default.png'
 import Gallery from '../img/gallery.png'
 
 class MainTweet extends Component {
@@ -11,10 +11,12 @@ class MainTweet extends Component {
   render() { 
     return ( 
       <div className="tweet">
+
         <div className="tweet-header">
-          <img className="tweet-profile-img" src={Logo} alt="profile_img"/>
+          <img className="tweet-profile-img" src={ProfileImg} alt="profile_img" />
           <textarea className="text-tweet" placeholder="무슨 일이 일어나고 있나요?"></textarea>
         </div>
+        
         <div className="tweet-footer">
           <img className="btn-gallery" src={Gallery} alt="gallery"></img>
           <input className="btn-tweet" type="button" value="트윗하기"/>
