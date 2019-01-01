6549975d7a91c9d7b93075a5af51953f26bfc620
huewilliams <4879sinwoo@naver.com>
2019-01-01 23:48:07 +0900

    CREATE: define user db

---
+++ BackEnd/code/models/User.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('User', {
+        id: {
+            type: DataTypes.STRING(20),
+            primaryKey: true
+        },
+        password: {
+            type: DataTypes.STRING(30),
+            allowNull: false
+        },
+        nickname: {
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        intro: {
+            type: DataTypes.STRING(50),
+            defaultValue: '자기소개가 없습니다. 자신을 다른 사람들에게 소개해보세요'
+        }
+    }, {
+        timestamps: false
+    })
+}
