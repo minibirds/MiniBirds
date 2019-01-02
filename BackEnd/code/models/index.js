ea318858c1a775558d1da60d8868a504e6ce130b
huewilliams <4879sinwoo@naver.com>
2019-01-02 10:15:30 +0900

    UPDATE: create relation of schemas

--- BackEnd/code/models/index.js
+++ BackEnd/code/models/index.js
@@ -16,4 +16,30 @@ const sequelize = new Sequelize(
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+db.User = require('./User')(sequelize, Sequelize);
+db.Post = require('./Post')(sequelize, Sequelize);
+db.Mention = require('./Mention')(sequelize, Sequelize);
+db.Follower = require('./Follower')(sequelize, Sequelize);
+db.Following = require('./Following')(sequelize. Sequelize);
+
+// User 과 Post 1 : N 관계 지정
+db.User.hasMany(db.Post, { foreignKey: 'userId', sourceKey: 'id' });
+db.Post.belongsTo(db.User, { foreignKey: 'userId', targetKey: 'id'});
+
+// User 과 Follower 1 : N 관계 지정
+db.User.hasMany(db.Follower, { foreignKey: 'userId', sourceKey: 'id' });
+db.Follower.belongsTo(db.User, { foreignKey: 'userId', targetKey: 'id'});
+
+// User 과 Follwing 1 : N 관계 지정
+db.User.hasMany(db.Following, { foreignKey: 'userId', sourceKey: 'id' });
+db.Following.belongsTo(db.User, { foreignKey: 'userId', targetKey: 'id'});
+
+// User 과 Mention 1 : N 관계 지정
+db.User.hasMany(db.Mention, { foreignKey: 'userId', sourceKey: 'id' });
+db.Mention.belongsTo(db.User, { foreignKey: 'userId', targetKey: 'id'});
+
+// Post 와 Mention 1 : N 관계 지정
+db.Post.hasMany(db.Mention, { foreignKey: 'postId', sourceKey: 'postId' });
+db.Mention.belongsTo(db.Post, { foreignKey: 'postId', targetKey: 'postId'});
+
 module.exports = db;
