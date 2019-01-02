e1c4dd21fe45a1f578e1372bb25002fd301ce0ee
soheejang15 <jsh167528@gmail.com>
2019-01-02 13:43:10 +0900

    SET : 라우터
    라우터 설정

--- FrontEnd/minibirds/src/App.js
+++ FrontEnd/minibirds/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Header from './Components/Header.js';
+import Main from './container/Main/Main';
+import Mypage from './container/Mypage/Mypage';
+import SignMain from './container/Account/SignMain';
+import MainService from './container/MainService/MainService';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
+        <BrowserRouter>
+          <React.Fragment>
+            <Header/>
+            <Switch>
+              <Route path='/' component={MainService} exact />
+              <Route path='/mypage' component={Mypage} exact />
+              <Route path='/account' component={SignMain} exact/>
+            </Switch>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
