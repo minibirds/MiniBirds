1a9a56c5cb466b80dcbae82506b1080af87d315a
huewilliams <4879sinwoo@naver.com>
2019-01-02 10:14:13 +0900

    CREATE: define Following Schema

---
+++ BackEnd/code/models/Following.js
@@ -0,0 +1,8 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('Following', {
+        FollowingId: {
+            type: DataTypes.STRING(20),
+            primaryKey: true
+        }
+    })
+};
