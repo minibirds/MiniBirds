8684393fd2a0b50b3cde05070fd7aca90e01fd91
soheejang15 <jsh167528@gmail.com>
2019-01-02 17:06:21 +0900

    MODIFY : 오타 수정

--- FrontEnd/minibirds/src/container/MainService/MainService.jsx
+++ FrontEnd/minibirds/src/container/MainService/MainService.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Main from '../Main/Main';
+import Mypage from '../Mypage/Mypage';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Header from '../../Components/Share/Header';
 
 const MainService = ({}) => {
     return(
         <div className="main-service">
-        </div>
+            <BrowserRouter>
+                <React.Fragment>
+                    <Header/>
+                    <Switch>
+                        <Route path='/' component={Main} exact />
+                        <Route path='/mypage' component={Mypage} exact/>
+                    </Switch>
+                </React.Fragment>
+            </BrowserRouter>
+        </div> 
     )
 }
 
