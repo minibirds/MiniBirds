6946c6ad25bd91aabf84e6220222c1b2b6da120a
soheejang15 <jsh167528@gmail.com>
2019-01-02 17:05:00 +0900

    SET : 라우터

--- FrontEnd/minibirds/src/App.js
+++ FrontEnd/minibirds/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Header from './Components/Header.js';
-import Main from './container/Main/Main';
-import Mypage from './container/Mypage/Mypage';
 import SignMain from './container/Account/SignMain';
 import MainService from './container/MainService/MainService';
 
@@ -13,10 +10,8 @@ class App extends Component {
       <div className="App">
         <BrowserRouter>
           <React.Fragment>
-            <Header/>
             <Switch>
               <Route path='/' component={MainService} exact />
-              <Route path='/mypage' component={Mypage} exact />
               <Route path='/account' component={SignMain} exact/>
             </Switch>
           </React.Fragment>
