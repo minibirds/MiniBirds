5f9478534ea6af16606770996bae5de46300e06a
soheejang15 <jsh167528@gmail.com>
2019-01-02 17:06:56 +0900

    SET : 마이페이지 라우터

--- FrontEnd/minibirds/src/container/Mypage/Mypage.jsx
+++ FrontEnd/minibirds/src/container/Mypage/Mypage.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import Profile from '../../Components/mypage/Profile';
 import TabBar from '../../Components/mypage/Tapba';
 import TwitList from '../../Components/Main/Twitlist';
+import FollowList from '../../Components/mypage/Followlist';
 import './Mypage.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 class Mypage extends Component {
   constructor(props) {
@@ -12,9 +14,16 @@ class Mypage extends Component {
   render() { 
     return ( 
       <div className="mypage-container">
-        <Profile/>
-        <TabBar/>
-        <TwitList/>
+        <BrowserRouter>
+          <React.Fragment>
+            <Profile/>
+            <TabBar/>
+            <Switch>
+              <Route path='/twitlist' component={TwitList} exact />
+              <Route path='/followlist' component={FollowList} exact />
+            </Switch>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
      );
   }
