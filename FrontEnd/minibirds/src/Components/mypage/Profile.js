505300c34f94419e9a7e3c551d26f767d518882b
soheejang15 <jsh167528@gmail.com>
2019-01-03 09:20:59 +0900

    MODIFY : 중복 className

--- FrontEnd/minibirds/src/Components/mypage/Profile.js
+++ FrontEnd/minibirds/src/Components/mypage/Profile.js
@@ -2,23 +2,22 @@ import React from 'react';
 import editimg from '../img/edit-img.png';
 import { Link } from 'react-router-dom';
 import './profile.css';
+import MyProfileImg from '../img/profile-img-default.png';
 
 const Profile = ({id, name, profileImg, info, Tapba}) => {
     return (
         <div>
-            <div className="main-profile">
-                <div className="big-box">
-                    <div className="small-box">
-                        <img src={profileImg} />
-                    </div>
-                </div>
+            <div className="detail-profile">
+                <img className="my-profile-img" src={MyProfileImg} alt=''/>
                 <div className="text-box">
                     <div className="nick-name">{name}히히낙낙</div>
                     <div className="user-id">{id}@dsasds</div>
                     <div className="info">{info}퉤ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ</div>
-                    <div className="edit-btn">
-                        <img src={editimg} alt="수정" className="edit-img" />
-                        <Link to='/mypage/edit'><div className="edit-letter">프로필 수정하기</div></Link>
+                    <div className="info-modify-btn">
+                        <Link to='/account/edit'>
+                            <img src={editimg} alt="수정" className="edit-img" />
+                            <span className="edit-letter">프로필 수정하기</span>
+                        </Link>
                     </div>
                 </div>
             </div>
