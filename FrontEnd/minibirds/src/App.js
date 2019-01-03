8759d40fd3def43011cba2b5ee134141db2543b0
milkyway002 <37265093+milkyway002@users.noreply.github.com>
2019-01-03 19:53:58 +0900

    ADD: 회원가입 페이지와 로그인 페이지 구분

--- FrontEnd/minibirds/src/App.js
+++ FrontEnd/minibirds/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import SignMain from './container/Account/SignMain.jsx';
+import SigninPage from './Components/Share/SigninPage';
 import MainService from './container/MainService/MainService';
+import SignupPage from './Components/Share/SignupPage';
 
 class App extends Component {
   render() {
@@ -12,7 +13,8 @@ class App extends Component {
           <React.Fragment>
             <Switch>
               <Route path='/' component={MainService} exact />
-              <Route path='/account' component={SignMain} exact/>
+              <Route path='/signin' component={SigninPage} exact/>
+              <Route path='/signup' component={SignupPage} exact/>
             </Switch>
           </React.Fragment>
         </BrowserRouter>
