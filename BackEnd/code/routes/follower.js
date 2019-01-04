48b5302557e15a848fe6360a6306ca9ae6f819af
huewilliams <4879sinwoo@naver.com>
2019-01-04 12:17:52 +0900

    CREATE: GET/ follower/:id
    id로 식별하여 follower 리스트를 가져오는 API

---
+++ BackEnd/code/routes/follower.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+let Follower = require('../models').Follower;
+const { verify } = require('./middlewares');
+let router = express.Router();
+router.use(cors());
+
+router.get('/:id', async (req, res)=>{
+    let err = {};
+    let token = req.cookies.sign;
+    try {
+        let auth = verify(token, 'entry_minibirds');
+        if (auth < 0) {
+            err.status = 401;
+            throw err;
+        }
+        if (auth == req.params.id) {
+            let list = await Follower.findAll({
+                attributes: ['FollowerId'],
+                where: {userId: req.params.id}
+            });
+            if(list) {
+                res.json({
+                    num: list.length,
+                    list
+                })
+            }
+        } else {
+            err.status = '401';
+            err.message = '권한이 없는 사용자 입니다';
+            throw err;
+        }
+    } catch (err) {
+        res.json({
+            status: err.status,
+            message: err.message
+        })
+    }
+});
+
+module.exports = router;
