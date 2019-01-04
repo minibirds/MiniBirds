84ca3f567f8d817109efc5d58e7a9490257c7182
soheejang15 <jsh167528@gmail.com>
2019-01-04 13:48:16 +0900

    MODIFY : 누락 파일 수정

--- FrontEnd/minibirds/src/Components/Share/SigninPage.jsx
+++ FrontEnd/minibirds/src/Components/Share/SigninPage.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
 import '../signup_in/Signin.css'
-import * as service from '../../service/get';
 import {Link} from 'react-router-dom';
+import Signlogo from '../signup_in/Signlogo';
+import Signin from '../signup_in/Signin';
 
-const Signin = ({}) => {
+const SigninPage = ({}) => {
     return (
-        <div className="input-wapper">
-            <div className="sign">SIGN IN</div>    
-            <div className="input-box">
-                <table>
-                    <tr>
-                        <td><div className="input-id">ID</div></td>
-                        <td><input className="input" /></td>
-                        <td rowSpan="2"><div className="log-in" /*onClick={() => Signinbtn()}*/>로그인</div></td>
-                    </tr>                    
-                    <tr>
-                        <td><div className="input-pw">PW</div></td>
-                        <td><input className="input" /></td>
-                    </tr>
-                </table>
-            </div>
-            <div className="info-box">
-                <div className="no-account">계정이 없으신가요?</div>
-                <Link to='/signup' ><div className="go-signin">회원가입</div></Link>
-            </div>
-            <div className="error-code">아이디 또는 비밀번호가 잘못되었습니다.</div>
+        <div className="background-box">
+            <div className="sign-container">
+                <div className="left-component">
+                    <Signlogo />
+                </div>
+                <div className="right-component">
+                    <Signin />
+                </div>       
+            </div> 
         </div>
     );
 }
 
-export default Signin;
+export default SigninPage;
