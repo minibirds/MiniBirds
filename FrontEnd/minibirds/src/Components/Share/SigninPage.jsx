32db5201d5273e7c1a4e7b3a9cc898c9a3669d52
milkyway002 <37265093+milkyway002@users.noreply.github.com>
2019-01-04 10:42:40 +0900

    ADD: 회원가입 ajax 통신 네트워크 에러때문에 막힘

--- FrontEnd/minibirds/src/Components/Share/SigninPage.jsx
+++ FrontEnd/minibirds/src/Components/Share/SigninPage.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import './SignPage.css';
-import {BrowserRouter, Route, Switch } from 'react-router-dom';
-import Signin from '../signup_in/Signin';
-import Signlogo from '../signup_in/Signlogo';
+import '../signup_in/Signin.css'
+import * as service from '../../service/get';
+import {Link} from 'react-router-dom';
 
-const SigninPage = () => {
+const Signin = ({}) => {
     return (
-        <div className="background-box">
-            <div className="sign-container">
-                <div className="left-component">
-                    <Signlogo />
-                </div>
-                <div className="right-component">
-                    <Signin />
-                </div>       
-            </div> 
+        <div className="input-wapper">
+            <div className="sign">SIGN IN</div>    
+            <div className="input-box">
+                <table>
+                    <tr>
+                        <td><div className="input-id">ID</div></td>
+                        <td><input className="input" /></td>
+                        <td rowSpan="2"><div className="log-in" /*onClick={() => Signinbtn()}*/>로그인</div></td>
+                    </tr>                    
+                    <tr>
+                        <td><div className="input-pw">PW</div></td>
+                        <td><input className="input" /></td>
+                    </tr>
+                </table>
+            </div>
+            <div className="info-box">
+                <div className="no-account">계정이 없으신가요?</div>
+                <Link to='/signup' ><div className="go-signin">회원가입</div></Link>
+            </div>
+            <div className="error-code">아이디 또는 비밀번호가 잘못되었습니다.</div>
         </div>
     );
 }
 
-export default SigninPage;
+export default Signin;
--- FrontEnd/minibirds/src/Components/signup_in/Signup.js
+++ FrontEnd/minibirds/src/Components/signup_in/Signup.js
@@ -1,37 +1,93 @@
-import React from 'react'
-import './Signup.css'
+import React, { Component } from 'react';
+import './Signup.css';
+import axios from 'axios';
 
-const Signup = ({}) => {
-    return (
-        <div className="input-wrapper">
-            <div className="sign">SIGN UP</div>  
-            <div>
-                <table className="input-table">
-                    <tr>
-                        <td><div className="input_set">NAME</div></td>
-                        <td><input className="input" /></td>
-                    </tr>
-                    <tr>
-                        <td><div className="input_set">ID</div></td>
-                        <td><input className="input" /></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><div className="error">사용할 수 없는 아이디입니다.</div></td>
-                    </tr>
-                    <tr>
-                        <td><div className="input_set">PW</div></td>
-                        <td><input type="password"  className="input" /></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><div className="error">입력하지 않은 정보가 있습니다.</div></td>
-                    </tr>
-                </table>
-                <div className="signin-btn">회원가입</div>
+class Signup extends Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            id: '',
+            password: '',
+            nickname: ''
+        };
+    }
+
+    ChangeInput = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    postUserInfo = () => {
+
+        axios({
+            method: 'post',
+            url: 'http://13.59.174.126:3000/auth/signup',
+            data: {
+                id: this.state.id,
+                password: this.state.password,
+                nickname: this.state.nickname
+            }
+        })
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+       /* axios.post('http://13.59.174.126:3000/auth/signup', {userInfo})
+        .then((res) => {
+            console.log(res.data);
+        }) */
+    }
+
+    render() {
+        const {
+            ChangeInput,
+            postUserInfo,
+        } = this;
+
+        const {
+            id,
+            password,
+            nickname,
+        } = this.state;
+
+        return (
+            <div className="input-wrapper">
+                <div className="sign">SIGN UP</div>  
+                <div>
+                    <table className="input-table">
+                    <tbody>
+                        <tr>
+                            <td><div className="input_set">NAME</div></td>
+                            <td><input className="input" value={nickname} onChange={ChangeInput} name="nickname" /></td>
+                        </tr>
+                        <tr>
+                            <td><div className="input_set">ID</div></td>
+                            <td><input className="input" value={id} onChange={ChangeInput} name="id" /></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><div className="error">사용할 수 없는 아이디입니다.</div></td>
+                        </tr>
+                        <tr>
+                            <td><div className="input_set">PW</div></td>
+                            <td><input type="password"  className="input" value={password} onChange={ChangeInput} name="password" /></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><div className="error">입력하지 않은 정보가 있습니다.</div></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <button onClick={() => postUserInfo()}className="signin-btn">회원가입</button>
+                </div>
             </div>
-        </div>
-    );
+        )
+    }
 }
 
 export default Signup;
