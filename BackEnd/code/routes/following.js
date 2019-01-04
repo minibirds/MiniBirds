cc549239ca6cab643a656ebbe538e2b9f6b18913
huewilliams <4879sinwoo@naver.com>
2019-01-04 09:46:57 +0900

    EDIT: POST/ following/
    다른 유저를 팔로우하는 API

--- BackEnd/code/routes/following.js
+++ BackEnd/code/routes/following.js
@@ -5,36 +5,55 @@ let Follower = require('../models').Follower;
 const { verify } = require('./middlewares');
 let router = express.Router();
 
-router.post('/', async (req, res)=>{
+router.post('/', (req, res)=>{
     let err = {};
     let token = req.cookies.sign;
     try {
         let auth = verify(token, 'entry_minibirds');
         if(auth < 0) throw err;
         if(auth == req.body.userId) {
-            let test = await Following.findOne({
-                where:{userId:req.body.userId, followingId:req.body.followingId}
+            Following.create({
+                FollowingId: req.body.targetId,
+                userId: req.body.userId
+            })
+                .then((followings)=>{
+                    console.log(followings.userId);
+                    Follower.create({
+                        FollowerId: req.body.userId,
+                        userId: req.body.targetId
+                    })
+                        .then((followers)=>{
+                            console.log(followers.userId);
+                            res.json({
+                                nick : followings.FollowingId,
+                            })
+                        })
+                })
+                .catch((err)=>{
+                    console.log(err);
+                    res.json(err);
+                })
+        }
+    } catch (err) {
+        res.json({status:err.status, message:err.message});
+    }
+});
+
+router.get('/:id', async (req, res)=>{
+    let err = {};
+    let token = req.cookies.sign;
+    try {
+        let auth = verify(token, 'entry_minibirds');
+        if(auth < 0) throw err;
+        if(auth == req.params.id) {
+            let list = await Following.findAll({
+                attributes: ['followingId'],
+                where: {userId: req.params.id}
             });
-            if(test) {
-                err.status = '409';
-                err.message = '이미 팔로우 중인 유저입니다';
-                throw err;
-            }
-            let following = await Following.create({
-                userId: req.body.userId,
-                FollowingId: req.body.followingId
-            });
-            let follower = await Follower.create({
-                userId: req.body.followingId,
-                FollowerId: req.body.userId
-            });
-            if(following || follower) {
-                let result = await Following.findAll({where: {userId : req.body.userId}});
-                let nick = await User.findOne({where: {id : req.body.followingId}});
-                console.log(result);
+            if(list) {
                 res.json({
-                    num : result.length,
-                    nick : nick.nickname
+                    num: list.length,
+                    list
                 })
             }
         } else {
@@ -49,7 +68,5 @@ router.post('/', async (req, res)=>{
         })
     }
 });
-
-
 
 module.exports = router;
