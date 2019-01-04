e9aa2bae70062ecedc47042c173f09b5a2e4d086
milkyway002 <37265093+milkyway002@users.noreply.github.com>
2019-01-04 10:57:39 +0900

    EDIT: css claaName 충돌 문제 해결

--- FrontEnd/minibirds/src/Components/mypage/Followlist.js
+++ FrontEnd/minibirds/src/Components/mypage/Followlist.js
@@ -4,17 +4,17 @@ import './followlist.css'
 const Followlist = ({profileImg, follow, nickName, Info}) => {
     return (
         <div>
-            <div className="back-box"></div>
+            <div className="background-box"></div>
             <div className="btn-box">
-                <div className="big-box">
-                    <div className="small-box">
+                <div className="behind-box">
+                    <div className="inner-box">
                             <img src={profileImg} />
                         </div>
                     </div>
                 <div className="follow-btn">{follow}팔로잉</div>
             </div>
-            <div className="letter-box">
-                <div className="user-name">{nickName}미미</div>
+            <div className="text-content">
+                <div className="user-name-text">{nickName}미미</div>
                 <div className="user-intro">{Info} ㅇㄴㅁ어ㅗㅓ놈너ㅗㅇ우아아아ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</div>
             </div>
         </div>
