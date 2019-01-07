91fa8f1eb303cbe806497bd91a5b57aacecfaaee
milkyway002 <37265093+milkyway002@users.noreply.github.com>
2019-01-07 20:01:39 +0900

    ADD: 트윗의 정보를 받아오는 axios get 코드 추가

--- FrontEnd/minibirds/src/container/MainPage/Main.jsx
+++ FrontEnd/minibirds/src/container/MainPage/Main.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Introduction from '../../Components/Main/Introduction';
 import MainTweet from '../../Components/Main/MainTweet';
 import MiniProfile from '../../Components/Main/MiniProfile';
 import TwitList from '../../Components/Share/Twitlist';
+import axios from 'axios';
+import base_url from '../../base_url';
 import './Main.css';
 
-const Main = ({CheckToken}) => {
-  return(
-    <div className='main-page'>
-      <MiniProfile/>
-      <div className="main-middle">
-        <MainTweet/>
-        <TwitList/>
+class Main extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      postID: '',
+      userID: '',
+      content: '',
+      img: '',
+      created_At: ''
+    }
+  }
+
+  componentDidMount() {
+    this.GetTwitInfo();
+  }
+
+  GetTwitInfo = () => {
+    axios.get(`${base_url}`)
+    .then((response) => {
+      this.setState({
+        postID: response.data.postID,
+        userID: response.data.userID,
+        content: response.data.content,
+        img: response.data.img,
+        created_At: response.data.created_At
+      });
+    })
+  }
+
+  render() {
+    return(
+      <div className='main-page'>
+        <MiniProfile/>
+        <div className="main-middle">
+          <MainTweet/>
+          <TwitList/>
+        </div>
+        <Introduction/>
       </div>
-      <Introduction/>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Main;
