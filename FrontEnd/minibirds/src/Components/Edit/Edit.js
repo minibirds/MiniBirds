3fb247f3586ccc499ac568d6d0a7ed91bccd9027
yoonju3221 <jyj_3221@naver.com>
2019-01-07 11:35:57 +0900

    ADD : 수정하기 기능 구현

--- FrontEnd/minibirds/src/Components/Edit/Edit.js
+++ FrontEnd/minibirds/src/Components/Edit/Edit.js
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Edit.css';
+import axios from 'axios';
+import base_url from '../../base_url';
 
-const Edit = ({id}) => {
+class Edit extends Component{
+    render() {
+        state = {
+            id: '',
+            pw: '',
+            nickname: '',
+            intro : '',
+        }
+        handleChange = (e) => {
+            this.setState({
+                [e.target.name] : e.target.value
+            })
+        }
+        handleModify = () => {
+            axios
+                .put(
+                    `${base_url}/edit/${this.state.id}`, {
+                        nickname: this.state.nickname,
+                        password: this.state.pw,
+                        intro: this.state.intro
+                    }, {
+                        header: {
+                            'Authorization': `bearer ${localStorage.getItem('token')}`,
+                        },
+                    })
+        }
     return (
         <div className="input-box">
             <table>
                 <tr>
                     <td><div className="input-letter">아이디</div></td>
-                    <td><div className="input-info">{id}</div></td>
+                    <td><div className="input-info">{this.state.id}</div></td>
                 </tr>
                 <tr>
                     <td><div className="input-letter">현재 비밀번호</div></td>
-                    <td><input className="input-info" /></td>
+                    <td><div className="input-info">{this.state.pw}</div></td>
                 </tr>
                 <tr>
                     <td><div className="input-letter">새 비밀번호</div></td>
-                    <td><input className="input-info" /></td>
+                     <td><input className="input-info" onChange={this.handleChange} value={this.state.pw} name="pw"/></td>
                 </tr>
                 <tr>
                     <td><div className="input-letter">닉네임</div></td>
-                    <td><input className="input-info" /></td>
+                    <td><input className="input-info" placeholder={this.state.nickname} onChange={this.handleChange} value={this.state.nickname} name="nickname"/></td>
                 </tr>
                 <tr>
                     <td><div className="input-letter">한줄소개</div></td>
-                    <td><textarea className="input-intro" /></td>
+                    <td><textarea className="input-intro" placeholder={this.state.intro} onChange={this.handleChange} value={this.state.intro} name="intro"/></td>
                 </tr>
             </table>
-            <div className="accept-edit-btn">수정하기</div>
+            <Link to={check? '/':'mypage'}>
+            <button className="accept-edit-btn" onClick={this.handleModify}>수정하기</button></Link>
+            
         </div>
     );
+    }
 }
 
 export default Edit;
