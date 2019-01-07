5d85b2752cde18594e0b68578575972a2423bbab
milkyway002 <37265093+milkyway002@users.noreply.github.com>
2019-01-07 20:02:53 +0900

    EDIT: 트윗 리스트 경로를 마이 트윗 리스트 경로로 수정

--- FrontEnd/minibirds/src/container/Mypage/Mypage.jsx
+++ FrontEnd/minibirds/src/container/Mypage/Mypage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import Profile from '../../Components/mypage/Profile';
 import TabBar from '../../Components/mypage/Tapba';
+import MyTwitList from '../MyTwitList/MyTwitList';
 import TwitList from '../../Components/Share/Twitlist';
 import FollowList from '../../Components/mypage/Followlist';
 import './Mypage.css';
@@ -36,7 +37,7 @@ class Mypage extends Component {
         <TabBar history={history} />
         <div className="mypage-twit-list" id="mypage-selected-list">
           {location.pathname === '/mypage/twitlist' ? 
-          <TwitList /> : location.pathname === '/mypage/following' ? <FollowList/> : <FollowList/>}
+          <MyTwitList /> : location.pathname === '/mypage/following' ? <FollowList/> : <FollowList/>}
         </div>
       </div>
      );
