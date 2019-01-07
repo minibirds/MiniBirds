e67e42ea15666b85f12a43a3f877dedb687f80db
milkyway002 <37265093+milkyway002@users.noreply.github.com>
2019-01-07 17:12:42 +0900

    ADD: 클릭 시 해당 카테고리 색상 활성화 코드 작성

--- FrontEnd/minibirds/src/Components/mypage/Tapba.js
+++ FrontEnd/minibirds/src/Components/mypage/Tapba.js
@@ -18,16 +18,16 @@ class Tapba extends Component {
         this.GetTwitNum();
         this.GetFollowerNum();
         this.GetFollowingNum();
-      }
+    }
 
     GetTwitNum = () => {
-        axios.get(`${base_url}/auth/twit/${this.state.id}`)
+        axios.get(`${base_url}/twit/${this.state.id}`)
         .then((response) => {
           this.setState({
             twitNumber: response.data.num
           });
         })
-      }
+    }
     
     GetFollowerNum = () => {
         axios.get(`${base_url}/follower/${this.state.id}`)
@@ -47,9 +47,86 @@ class Tapba extends Component {
         })
     }
 
+    handleTwitColorChange = () => {
+        const twitColor = document.getElementById('table-twit');
+        const FollowingColor = document.getElementById('table-following');
+        const FollowerColor = document.getElementById('table-follower');
+        const twitNumColor = document.getElementById('twit-number');
+        const FollowingNumColor = document.getElementById('following-number');
+        const FollowerNumColor = document.getElementById('follower-number');
+
+        if (twitColor.className === 'table-twit') {
+            twitColor.className = 'on-table-twit';
+            FollowerColor.className = 'table-follower';
+            FollowingColor.className = 'table-following';
+            twitNumColor.className = 'on-twit-number';
+            FollowerNumColor.className = 'follower-number';
+            FollowingNumColor.className = 'following-number';
+        }
+    }
+
+    handleFollowingColorChange = () => {
+        const twitColor = document.getElementById('table-twit');
+        const FollowingColor = document.getElementById('table-following');
+        const FollowerColor = document.getElementById('table-follower');
+        const twitNumColor = document.getElementById('twit-number');
+        const FollowingNumColor = document.getElementById('following-number');
+        const FollowerNumColor = document.getElementById('follower-number');
+
+        if (FollowingColor.className === 'table-following') {
+            FollowingColor.className = 'on-table-following';
+            twitColor.className = 'table-twit';
+            FollowerColor.className = 'table-follower';
+            twitNumColor.className = 'twit-number';
+            FollowerNumColor.className = 'follower-number';
+            FollowingNumColor.className = 'on-following-number';
+        }
+    }
+
+    handleFollowerColorChange = () => {
+        const twitColor = document.getElementById('table-twit');
+        const FollowingColor = document.getElementById('table-following');
+        const FollowerColor = document.getElementById('table-follower');
+        const twitNumColor = document.getElementById('twit-number');
+        const FollowingNumColor = document.getElementById('following-number');
+        const FollowerNumColor = document.getElementById('follower-number');
+
+        if (FollowerColor.className === 'table-follower') {
+            console.log(1);
+            FollowingColor.className = 'table-following';
+            twitColor.className = 'table-twit';
+            FollowerColor.className = 'on-table-follower';
+            twitNumColor.className = 'twit-number';
+            FollowerNumColor.className = 'on-follower-number';
+            FollowingNumColor.className = 'following-number';
+        }
+    }
+
+    onClickTwitList = () => {
+        const { history } = this.props;
+        history.push('/mypage/twitlist');
+        this.handleTwitColorChange();
+    }
+
+    onClickFollower = () => {
+        const { history } = this.props;
+        history.push('/mypage/followers');
+        this.handleFollowerColorChange();
+    }
+
+    onClickFollowing = () => {
+        const { history } = this.props;
+        history.push('/mypage/following');
+        this.handleFollowingColorChange();
+    }
+
     render() {
         const { twitNum, followerNum, followingNum } = this.state;
-        const { history } = this.props;
+        const {
+            onClickTwitList,
+            onClickFollower,
+            onClickFollowing,
+        } = this;
 
         return (
             <div className="tap-bar">
@@ -57,14 +134,14 @@ class Tapba extends Component {
                     <table className="letter-table">
                         <tbody>
                             <tr className="first-table">
-                                <td onClick={() => history.push('/mypage/twitlist')} className="table-twit">나의 트윗</td>
-                                <td onClick={() => history.push('/mypage/following')}  className="table-following">팔로잉</td>
-                                <td onClick={() => history.push('/mypage/followers')} className="table-follower">팔로워</td>
+                                <td onClick={() => onClickTwitList()} className="table-twit" id="table-twit">나의 트윗</td>
+                                <td onClick={() => onClickFollowing()}  className="table-following" id="table-following">팔로잉</td>
+                                <td onClick={() => onClickFollower()} className="table-follower" id="table-follower">팔로워</td>
                             </tr>
                             <tr className="second-table">
-                                <td className="twit-number" onClick={() => history.push('/mypage/twitlist')}>{twitNum}</td>
-                                <td className="following-number" onClick={() => history.push('/mypage/following')}>{followingNum}</td>
-                                <td className="follower-number" onClick={() => history.push('/mypage/followers')}>{followerNum}</td>
+                                <td className="twit-number" id="twit-number" onClick={() => onClickTwitList()}>1</td>
+                                <td className="following-number" id="following-number" onClick={() => onClickFollowing()}>1</td>
+                                <td className="follower-number" id="follower-number" onClick={() => onClickFollower()}>s1</td>
                             </tr>
                         </tbody>
                     </table>
