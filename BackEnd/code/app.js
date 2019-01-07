66f0e06af2d3ee441c56a7ea537f091030c6e368
huewilliams <4879sinwoo@naver.com>
2019-01-07 14:20:41 +0900

    EDIT: connect path (following router)

--- BackEnd/code/app.js
+++ BackEnd/code/app.js
@@ -39,7 +39,7 @@ app.use(session({
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/twit', twitRouter);
-app.use('/',followingRouter);
+app.use('/following',followingRouter);
 app.use('/',followerRouter);
 app.use('/profile', profileRouter);
 
