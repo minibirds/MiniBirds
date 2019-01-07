004428e6bb47303bc4ba0f2dd50893d81181af65
soheejang15 <jsh167528@gmail.com>
2019-01-07 14:39:50 +0900

    MODIFY : 정보 수정 페이지 이동 Link 태그

--- FrontEnd/minibirds/src/Components/mypage/Profile.js
+++ FrontEnd/minibirds/src/Components/mypage/Profile.js
@@ -4,6 +4,7 @@ import './profile.css';
 import MyProfileImg from '../img/profile-img-default.png';
 import axios from 'axios';
 import base_url from '../../base_url';
+import {Link} from 'react-router-dom';
 
 class Profile extends Component {
 
@@ -44,10 +45,10 @@ class Profile extends Component {
                         <div className="nick-name">{nickName}</div>
                         <div className="user-id">{userID}</div>
                         <div className="info">{info}</div>
-                        <div className="info-modify-btn">
+                        <Link to='/edit' className="info-modify-btn">
                             <img src={editimg} alt="수정" className="edit-img" />
                             <span className="edit-letter">프로필 수정하기</span>
-                        </div>
+                        </Link>
                     </div>
                 </div>
             </div>
