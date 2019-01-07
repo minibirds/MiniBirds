cbeef86b9b382f7e4b336d749781aa1dfa95fa87
soheejang15 <jsh167528@gmail.com>
2019-01-07 09:45:01 +0900

    ADD : 토큰 검사 함수

--- FrontEnd/minibirds/src/App.js
+++ FrontEnd/minibirds/src/App.js
@@ -7,16 +7,29 @@ import SignupPage from './Components/Share/SignupPage';
 import Mypage from './container/Mypage/Mypage';
 
 class App extends Component {
+  CheckToken = (history) => {
+    if(localStorage.getItem('token') === ''){
+      
+      return history;
+    }
+  }
+
+  constructor(){
+    super();
+    this.CheckToken();
+}
+  
   render() {
+
     return (
       <div className="App">
         <BrowserRouter>
           <React.Fragment>
             <Switch>
-              <Route path='/' component={MainService} exact />
+              <Route path='/' component={() => <MainService CheckToken={this.CheckToken} history={this.history}/>} exact />
               <Route path='/signin' component={SigninPage} exact/>
               <Route path='/signup' component={SignupPage} exact/>
-              <Route path='/mypage' component={Mypage} />
+              <Route path='/mypage' component={() => <Mypage CheckToken={this.CheckToken} history={this.history} />} />
             </Switch>
           </React.Fragment>
         </BrowserRouter>
@@ -26,3 +39,4 @@ class App extends Component {
 }
 
 export default App;
+
