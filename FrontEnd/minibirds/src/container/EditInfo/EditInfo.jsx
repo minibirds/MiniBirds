d111d7f50b24cbf135d4109064481d0defd3b9ff
soheejang15 <jsh167528@gmail.com>
2019-01-07 13:55:08 +0900

    ADD : 정보 수정 페이지 컴포넌트 추가

---
+++ FrontEnd/minibirds/src/container/EditInfo/EditInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EditImg from '../../Components/Edit/Editimg';
+import Edit from '../../Components/Edit/Edit';
+import './EditInfo.css';
+
+const EditInfo = () => (
+  <div className="edit-container">
+    <EditImg/>
+    <Edit/>
+  </div>
+);
+
+export default EditInfo;
