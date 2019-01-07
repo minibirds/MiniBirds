17f252fecaeed9947ee62bbc40a79c3fa03e4196
huewilliams <4879sinwoo@naver.com>
2019-01-07 10:32:13 +0900

    UPDATE: connect profile router

--- BackEnd/code/app.js
+++ BackEnd/code/app.js
@@ -9,6 +9,7 @@ const authRouter = require('./routes/auth');
 const twitRouter = require('./routes/twit');
 const followingRouter = require('./routes/following');
 const followerRouter = require('./routes/follower');
+const profileRouter = require('./routes/profile');
 const sequelize = require('./models').sequelize;
 
 const app = express();
@@ -40,6 +41,7 @@ app.use('/auth', authRouter);
 app.use('/twit', twitRouter);
 app.use('/following',followingRouter);
 app.use('/follower',followerRouter);
+app.use('/profile', profileRouter);
 
 // 해당 라우터가 없을시 404 Error 발생
 app.use((req, res, next)=>{
