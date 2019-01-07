414c308672d697555ef895cbb61cfd09e69fbb73
soheejang15 <jsh167528@gmail.com>
2019-01-07 13:57:09 +0900

    CHANGE : 클래스형 컴포넌트로 변경

--- FrontEnd/minibirds/src/container/MainService/MainService.jsx
+++ FrontEnd/minibirds/src/container/MainService/MainService.jsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Main from '../MainPage/Main';
 import Mypage from '../Mypage/Mypage';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, withRouter} from 'react-router-dom';
 import Header from '../../Components/Share/Header';
 import Twit from '../../Components/Main/MainTweet';
 
-const MainService = ({CheckToken, history}) => {
-    return(
-        <div className="main-service">
-            <BrowserRouter>
-                <React.Fragment>
-                    <Header CheckToken={CheckToken} history={history} />
-                    <Switch>
-                        <Route path='/' component={Main} exact />
-                        <Route path='/mypage' component={Mypage} exact/>
-                    </Switch>
-                </React.Fragment>
-            </BrowserRouter>
-        </div> 
-    )
+class MainService extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    componentDidMount(){
+        this.CheckToken();
+    }
+
+    CheckToken = () => {
+        const {history} = this.props;
+        if(localStorage.getItem('token') === ''){
+            history.push('/');
+        }
+        
+    }
+
+    render() { 
+        const {history} = this.props;
+        return(
+            <div className="main-service">
+                <BrowserRouter>
+                    <React.Fragment>
+                        <Header history={history} />
+                        <Switch>
+                            <Route path='/' component={Main} exact />
+                            <Route path='/mypage' component={Mypage} exact/>
+                        </Switch>
+                    </React.Fragment>
+                </BrowserRouter>
+            </div> 
+        )
+    }
 }
 
-export default MainService;
+export default withRouter(MainService);
