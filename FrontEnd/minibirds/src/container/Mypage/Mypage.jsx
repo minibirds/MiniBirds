cf6641c82ab41b9ed647e6390a40e9998f3a21b1
soheejang15 <jsh167528@gmail.com>
2019-01-09 08:50:08 +0900

    ADD : 리스트 토글 함수

--- FrontEnd/minibirds/src/container/Mypage/Mypage.jsx
+++ FrontEnd/minibirds/src/container/Mypage/Mypage.jsx
@@ -6,6 +6,8 @@ import MyTwitList from "../MyTwitList/MyTwitList";
 import "./Mypage.css";
 import Header from "../../Components/Share/Header";
 import MainFollowerList from "../followerList/MainfollowerList";
+import axios from 'axios';
+import base_url from '../../base_url';
 
 class Mypage extends Component {
   constructor(props) {
@@ -19,6 +21,36 @@ class Mypage extends Component {
 
   componentDidMount() {
     this.CheckToken();
+    this.handleConnect();
+  }
+
+  ListToggle = (TargetClass) => {
+    const { twitList, followingList, followerList } = this.state;
+    switch(TargetClass){
+      case 'table-twit':
+        this.setState({
+          twitList: true,
+          followingList: false,
+          followerList: false,
+        });
+        break;
+      case 'table-follower':
+      this.setState({
+        twitList: false,
+        followingList: false,
+        followerList: true,
+      });
+        break;
+      case 'table-following':
+      this.setState({
+        twitList: false,
+        followingList: true,
+        followerList: false,
+      });
+        break;  
+      default:
+        break;  
+    }
   }
 
   CheckToken = () => {
@@ -28,14 +60,38 @@ class Mypage extends Component {
     }
   };
 
+  handleConnect = () => {
+    axios
+      .get(`${base_url}/auth/token`, {
+        headers: {
+          token: `${localStorage.getItem("token")}`
+        }
+      })
+      .then(res => {
+        this.props.getUserData(
+          res.data.id,
+          res.data.nickname,
+          res.data.password,
+          res.data.intro
+        );
+      })
+      .catch(res => {
+        this.setState({
+          isError: true
+        });
+        console.log(res);
+        console.log("에러");
+      });
+  };
+
   render() {
-    const { twitList, followingList } = this.state;
+    const { twitList, followingList, followerList } = this.state;
     const { history, nickname, id, intro } = this.props;
     return (
       <div className="mypage-container">
         <Header />
         <Profile id={id} nickname={nickname} intro={intro} />
-        <TabBar history={history} />
+        <TabBar history={history} ListToggle={this.ListToggle} />
         <div className="mypage-twit-list" id="mypage-selected-list">
           {twitList ? (
             <MyTwitList nickname={nickname} />
