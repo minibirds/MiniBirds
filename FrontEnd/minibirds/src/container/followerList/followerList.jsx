cf0875f4474f2d41aaa49ee1f14b4884c8576754
milkyway002 <37265093+milkyway002@users.noreply.github.com>
2019-01-08 19:07:26 +0900

    CREATE: 팔로잉/팔로워 정렬을 위해 컨테이너 생성

---
+++ FrontEnd/minibirds/src/container/followerList/followerList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class FollowerList extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default FollowerList;
