6fe422f027fe7cca97772494437b09f964383b16
milkyway002 <37265093+milkyway002@users.noreply.github.com>
2019-01-08 20:11:46 +0900

    UPDATE: 불필요한 코드 제거

--- FrontEnd/minibirds/src/container/EditInfo/EditInfo.jsx
+++ FrontEnd/minibirds/src/container/EditInfo/EditInfo.jsx
@@ -4,9 +4,7 @@ import Edit from "../../Components/Edit/Edit.jsx";
 import "./EditInfo.css";
 
 class EditInfo extends Component {
-  constructor(props) {
-    super(props);
-  }
+
   render() {
     const { id, nickname, pw, intro, history } = this.props
     return (
