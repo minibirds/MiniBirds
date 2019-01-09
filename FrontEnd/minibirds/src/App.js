41140edce8475766c711782564afebc9023ccfa7
soheejang15 <jsh167528@gmail.com>
2019-01-09 12:12:22 +0900

    ADD : props추가

--- FrontEnd/minibirds/src/App.js
+++ FrontEnd/minibirds/src/App.js
@@ -1,55 +1,76 @@
-import React, { Component } from 'react';
-import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import SigninPage from './Components/Share/SigninPage';
-import MainService from './container/MainService/MainService';
-import SignupPage from './Components/Share/SignupPage';
-import Mypage from './container/Mypage/Mypage';
-import EditInfo from './container/EditInfo/EditInfo';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import SigninPage from "./Components/Share/SigninPage";
+import MainService from "./container/MainService/MainService";
+import SignupPage from "./Components/Share/SignupPage";
+import Mypage from "./container/Mypage/Mypage";
+import EditInfo from "./container/EditInfo/EditInfo";
 
 class App extends Component {
   state = {
-    id: '',
-    nickname: '',
-    pw: '',
-    intro: '',
-  }
+    id: "",
+    nickname: "",
+    pw: "",
+    intro: ""
+  };
   getUserData = (id, nickname, pw, intro) => {
     this.setState({
-      ...this.state,
       id,
       nickname,
       pw,
-      intro,
+      intro
     });
-  }
+  };
+  
   render() {
-
+    const { id, nickname, pw, intro } = this.state;
     return (
       <div className="App">
         <BrowserRouter>
           <React.Fragment>
             <Switch>
-              <Route path='/' component={() => <MainService history={this.history}/>} exact />
               <Route
-                path='/signin'
-                component={() => <SigninPage history={this.history} signin={this.getUserData}/>}
+                path="/"
+                render={() => <MainService history={this.history} id={id} nickname={nickname}  />}
                 exact
               />
-              <Route path='/signup' component={SignupPage} exact/>
-              <Route path='/mypage' component={() => <Mypage history={this.history} getUserData={this.getUserData} />} />
               <Route
-                path='/edit'
-                component={() =>
-                    <EditInfo 
-                      id={this.state.id}
-                      nickname={this.state.nickname}
-                      pw={this.state.pw}
-                      intro={this.state.intro}
-                      history={this.history}
-                    />
-                  }
-                />
+                path="/signin"
+                component={() => (
+                  <SigninPage
+                    history={this.history}
+                    signin={this.getUserData}
+                  />
+                )}
+                exact
+              />
+              <Route path="/signup" component={SignupPage} exact />
+              <Route
+                path="/mypage"
+                render={() => (
+                  <Mypage
+                    history={this.history}
+                    getUserData={this.getUserData}
+                    id={id}
+                    nickname={nickname}
+                    pw={pw}
+                    intro={intro}
+                  />
+                )}
+              />
+              <Route
+                path="/edit"
+                component={() => (
+                  <EditInfo
+                    id={id}
+                    nickname={nickname}
+                    pw={pw}
+                    intro={intro}
+                    history={this.history}
+                  />
+                )}
+              />
             </Switch>
           </React.Fragment>
         </BrowserRouter>
@@ -59,4 +80,3 @@ class App extends Component {
 }
 
 export default App;
-
